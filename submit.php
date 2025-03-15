201574e7cf982809483b0ab2d6b09599ab0d9727
Shoikot <farahabi.shoikot32@gmail.com>
2025-03-15 13:21:08 +0600

    1st Commit

---
+++ submit.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+    <title>Home</title>
+
+    </head>
+<body>
+
+    <h1> Registration Compplete :) </h1>
+    <h2> Thank you for registering with ABC Airlines. </h2>
+    <h3> We will send you a confirmation email shortly. </h3>
+    
+   
+
+
+    
+</body>
+
+</html>
